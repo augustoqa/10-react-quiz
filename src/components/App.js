1f1b4789367bcef003866012f20e834f2d12e5bc
Augusto Acual <augustoacual@gmail.com>
2026-04-14 11:00:38 -0600

    Display Questions

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ const initialState = {
   questions: [],
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: 'loading',
+  index: 0,
 }
 
 function reducer(state, action) {
@@ -37,7 +38,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState)
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  )
 
   const numQuestions = questions.length
 
@@ -58,7 +62,7 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question />}
+        {status === 'active' && <Question question={questions[index]} />}
       </Main>
     </div>
   )
